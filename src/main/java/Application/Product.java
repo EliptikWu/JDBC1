90ba0a608af5796516b2beeca6a3ef384e5ac766
EliptikWu <114369605+EliptikWu@users.noreply.github.com>
2024-02-27 15:55:01 -0500

    1.0

---
+++ src/main/java/Application/Product.java
@@ -0,0 +1,18 @@
+package Application;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+    private LocalDate dateRegistration;
+
+}
